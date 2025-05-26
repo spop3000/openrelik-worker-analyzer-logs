6cad4e2ccf01274aee1a2f3a7b2e81ec29d3f0b0
spop3000 <spop@google.com>
2025-05-26 08:20:57 +0000

    Fix the display_name

--- src/tasks.py
+++ src/tasks.py
@@ -25,7 +25,7 @@ TASK_NAME = "openrelik-worker-analyzer-logs.tasks.ssh_analyzer"
 
 # Task metadata for registration in the core system.
 TASK_METADATA = {
-    "display_name": "pyproject",
+    "display_name": "SSH login analyzer",
     "description": "Search for suspicious SSH login events in system logs",
     # Configuration that will be rendered as a web for in the UI, and any data entered
     # by the user will be available to the task function when executing (task_config).
@@ -101,5 +101,6 @@ def run_ssh_analyzer(
     return create_task_result(
         output_files=output_files,
         workflow_id=workflow_id,
+        task_report=task_report.to_dict(),
         meta={},
     )
